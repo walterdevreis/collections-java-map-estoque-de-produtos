3e2f323b284618b50457ae0448c0f20823812843
walter dev reis <walter-reis@outlook.com>
2025-09-24 21:56:48 -0300

    Criada e testada a classe Produto

---
+++ src/entities/Produto.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Produto {
+
+    private String nome;
+    private int quantidade;
+    private double preco;
+
+    public Produto(){}
+
+    public Produto(String nome, int quantidade, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", quantidade=" + quantidade +
+                ", preco=" + preco +
+                '}';
+    }
+}
